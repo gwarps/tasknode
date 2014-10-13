e3b487bfb2b41745eec6abe1c5037c5d368baede
baseplate <puneet.devel@gmail.com>
2014-10-13 21:10:47 +0530

    models validated against jslint

--- models/task.js
+++ models/task.js
@@ -1,13 +1,13 @@
-var mongoose = require('mongoose')
-   ,Schema = mongoose.Schema
-   ,ObjectId = Schema.ObjectId;
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
 
 var taskSchema = new Schema({
-   taskCode: {type: String},
-   taskSubCode: {type: String},
-   taskDesc: {type: String},
-   created: {type: Date},
-   updated: {type: Date}
-});
+        taskCode: {type: String},
+        taskSubCode: {type: String},
+        taskDesc: {type: String},
+        created: {type: Date},
+        updated: {type: Date}
+    });
 
 module.exports = mongoose.model('Task', taskSchema);
