4217c223e6054225f1b23c35bf2b84aaca92591b
baseplate <puneet.devel@gmail.com>
2014-10-14 23:10:01 +0530

    jslint correction in routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,114 +2,110 @@ var ObjectID = require('mongodb').ObjectID;
 
 var Post = require('../models/post');
 
-module.exports = function(app) {
-   /**
-   * GET all posts
-   **/
-   app.get("/posts", function(req, res, next) {
-      Post.find({}, function(err, docs) {
-         if (err) throw err;
-         res.render("posts", {"posts" : docs});    
-      });
-   });
+module.exports = function (app) {
+    "use strict";
+    /**
+    * GET all posts
+    **/
+    app.get("/posts", function (req, res, next) {
+        Post.find({}, function (err, docs) {
+            if (err) { throw err; }
+            res.render("posts", {"posts": docs});
+        });
+    });
 
-   /**
-   * GET a post by id, where id is object id (_id)
-   **/
-   app.get("/post/edit/:id", function(req, res, next) {
-     "use strict";  
-   
-      var query = {_id: req.params.id};
+    /**
+    * GET a post by id, where id is object id (_id)
+    **/
+    app.get("/post/edit/:id", function (req, res, next) {
+        var query = {_id: req.params.id};
 
-      Post.findOne(query, function(err, doc) {
-         if (err) throw err;
+        Post.findOne(query, function (err, doc) {
+            if (err) { throw err; }
 
-         doc.tags = doc.tags.join().toString();
-         res.render("post_edit", {"post" : doc});
-      });
-   });
+            doc.tags = doc.tags.join().toString();
+            res.render("post_edit", {"post" : doc});
+        });
+    });
 
-   /**
-   * POST data for a post to create a post
-   **/
-   app.post("/post", function(req, res, next) {
-      "use strict";
-      var post = new Post();
+    /**
+    * POST data for a post to create a post
+    **/
+    app.post("/post", function (req, res, next) {
+        var post = new Post(),
+            cleaned = [],
+            tags_array = req.body.postTags.split(","),
+            i;
 
-      post.taskCode = req.body.taskCode;
-      post.taskSubCode = req.body.taskSubCode;
-      post.postText = req.body.postText;
-      //post.tags = req.body.postTags;
+        post.taskCode = req.body.taskCode;
+        post.taskSubCode = req.body.taskSubCode;
+        post.postText = req.body.postText;
+        //post.tags = req.body.postTags;
 
-      post.author = ObjectID(1);
-      post.created = new Date();
-      post.updated = new Date();
+        post.author = new ObjectID(1);
+        post.created = new Date();
+        post.updated = new Date();
 
-      var cleaned = [];
-      var tags_array = req.body.postTags.split(",");
 
-      for (var i = 0; i < tags_array.length; i++) {
-         if((cleaned.indexOf(tags_array[i]) == -1) && tags_array[i] != "") {
-            cleaned.push(tags_array[i].replace(/\s/g,''));
-         }
-      }
-     
-      post.tags = cleaned;
-      
-      post.save(function(err, doc, noDocAffected) {
-         if (err) {
-            throw err;
-         } else {
+        for (i = 0; i < tags_array.length; i += 1) {
+            if ((cleaned.indexOf(tags_array[i]) === -1) && tags_array[i] !== "") {
+                cleaned.push(tags_array[i].replace(/\s/g, ''));
+            }
+        }
+
+        post.tags = cleaned;
+
+        post.save(function (err, doc, noDocAffected) {
+            if (err) {
+                throw err;
+            }
             console.log("pending impl...");
-         }
-         console.log("redirection pending");
-      });
+            console.log("redirection pending");
+        });
 
-   });
+    });
 
-   app.post("/post/edit/:id", function(req, res, next) {
-      "use strict";
-      var taskCode = req.body.taskCode;
-      var taskSubCode = req.body.taskSubCode;
-      var postText = req.body.postText;
-      var postTags = req.body.postTags;
- 
-    
-      var cleaned = [];
+    app.post("/post/edit/:id", function (req, res, next) {
+        var taskCode = req.body.taskCode,
+            taskSubCode = req.body.taskSubCode,
+            postText = req.body.postText,
+            postTags = req.body.postTags,
+            cleaned = [],
+            tags_array,
+            i,
+            update;
 
-      var tags_array = postTags.split(",");
+        tags_array = postTags.split(",");
 
-      for (var i = 0; i < tags_array.length; i++) {
-         if((cleaned.indexOf(tags_array[i]) == -1) && tags_array[i] != "") {
-            cleaned.push(tags_array[i].replace(/\s/g,''));
-         }
-      }
-      
-      var update = {
-                      "taskCode": req.body.taskCode,
-                      "taskSubCode": req.body.taskSubCode,
-                      "postText": req.body.postText,
-                      "tags": cleaned,
-                      "updated": new Date()
+        for (i = 0; i < tags_array.length; i += 1) {
+            if ((cleaned.indexOf(tags_array[i]) === -1) && tags_array[i] !== "") {
+                cleaned.push(tags_array[i].replace(/\s/g, ''));
+            }
+        }
 
-                   };
+        update = {
+            "taskCode": req.body.taskCode,
+            "taskSubCode": req.body.taskSubCode,
+            "postText": req.body.postText,
+            "tags": cleaned,
+            "updated": new Date()
+        };
 
-      Post.findByIdAndUpdate(req.params.id, update, function(err, doc) {
-        if(err) throw err;
-        console.log(doc + "updated");
-        res.redirect("/posts"); 
-      });
-   });
+        Post.findByIdAndUpdate(req.params.id, update, function (err, doc) {
+            if (err) { throw err; }
+            console.log(doc + "updated");
+            res.redirect("/posts");
+        });
+    });
 
-   /**
-   * DELETE a post from post list
-   **/
-   app.delete("/post/:id", function(req, res, next) {
-      "use strict";
-       Post.findByIdAndRemove(req.params.id, function(err) {
-          if (err) throw err;
-          console.log("redirection pending");
-       });
-   });
+    /**
+    * DELETE a post from post list
+    **/
+    app.delete("/post/:id", function (req, res, next) {
+        Post.findByIdAndRemove(req.params.id, function (err) {
+            if (err) { throw err; }
+            console.log("redirection pending");
+        });
+    });
 
-}
+};
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,54 +1,54 @@
 var Task = require("../models/task");
 
 
-module.exports = function(app) {
+module.exports = function (app) {
+    "use strict";
+    app.get("/tasks", function (req, res, next) {
+        Task.find({}, function (err, docs) {
+            if (err) { throw err; }
+            res.render("tasks", {"tasks": docs, message: req.flash('info')});
+        });
+    });
 
-   app.get("/tasks", function(req, res, next) {
-      Task.find({}, function(err, docs) {
-         if (err) throw err;
-         res.render("tasks", {"tasks": docs, message: req.flash('info')});
-      });
-   });
+    app.get("/task/:id", function (req, res, next) {
+        var query = {_id: req.params.id};
+        Task.findOne(query, function (err, doc) {
+            if (err) { throw err; }
+            res.render("task", {"task": doc});
+        });
+    });
 
-   app.get("/task/:id", function(req, res, next) {
-      var query = {_id: req.params.id};
-      Task.findOne(query, function(err, doc) {
-         if (err) throw err;
-         res.render("task", {"task": doc});
-      });
-   });
+    app.post("/task", function (req, res, next) {
+        var task = new Task();
 
-   app.post("/task", function(req, res, next) {
-      var task = new Task();
+        task.taskCode = req.body.taskCode;
+        task.taskSubCode = req.body.taskSubCode;
+        task.taskDesc = req.body.taskDescription;
+        task.created = new Date();
+        task.updated = new Date();
 
-      task.taskCode = req.body.taskCode;
-      task.taskSubCode = req.body.taskSubCode;
-      task.taskDesc = req.body.taskDescription;
-      task.created = new Date();
-      task.updated = new Date();
+        //console.log(task);
 
-      console.log(task);
-
-      task.save(function(err, doc, noDocsAffected) {
-         if (err) {
-            req.flash('info', 'Oops. seems to be some problem: ' + err);
-            console.log(err);
-         } else {
-            req.flash('info', 'Task created under task code: ' + task.taskCode);
-         }
-         res.redirect('/tasks');
-      });
-   });
+        task.save(function (err, doc, noDocsAffected) {
+            if (err) {
+                req.flash('info', 'Oops. seems to be some problem: ' + err);
+                console.log(err);
+            } else {
+                req.flash('info', 'Task created under task code: ' + task.taskCode);
+            }
+            res.redirect('/tasks');
+        });
+    });
 
 
    /**
    Delete task from the task list
    **/
-   app.delete("/task/:id", function(req, res, next) {
-      Task.findByIdAndRemove(req.params.id, function(err) {
-         if (err) throw err;
-         console.log("redirection pending again");
-      }); 
-   }); 
+    app.delete("/task/:id", function (req, res, next) {
+        Task.findByIdAndRemove(req.params.id, function (err) {
+            if (err) { throw err; }
+            console.log("redirection pending again");
+        });
+    });
 
-}
+};
