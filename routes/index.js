3f649d48c9b6347ce9693152db1c68687c2a69d2
Puneet Singh <puneet.mir@gmail.com>
2014-10-14 23:10:01 +0530

    jslint correction in routes

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
-module.exports = exports = function(app) {
-   require("./tasks")(app);
-   require("./posts")(app);
+module.exports = exports = function (app) {
+    "use strict";
+    require("./tasks")(app);
+    require("./posts")(app);
 
-   app.get("/", function(req, res) {
+    app.get("/", function (req, res) {
       //var er = req.flash("error", "it worked");
       //res.redirect('docs', {"message" : er});
-      res.render('index', { message : req.flash('info') });
-   });
+        res.render('index', { message : req.flash('info') });
+    });
 
-   app.get("/docs", function(req, res) {
-      res.render('docs');
-   });
+    app.get("/docs", function (req, res) {
+        res.render('docs');
+    });
 
-   app.get('/flash', function(req, res) {
-      req.flash('info', 'Hi There');
-      res.redirect('/');
-   });
-}
+    app.get('/flash', function (req, res) {
+        req.flash('info', 'Hi There');
+        res.redirect('/');
+    });
+};
