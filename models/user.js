6d83e4d50511b55fd205c33b406d1834efff9f53
Puneet Singh <puneet.mir@gmail.com>
2014-10-16 21:28:06 +0530

    user account serializer in mongoose schema

--- models/user.js
+++ models/user.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose'),
+    bcrypt = require('bcrypt-nodejs'),
     Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 
 var userSchema = new Schema({
-        userName: {type: String}, //will be email
+        email: {type: String}, //will be email
         firstName: {type: String},
         lastName: {type: String},
         gender: {type: String},
@@ -11,4 +12,15 @@ var userSchema = new Schema({
         password: {type: String}
     });
 
+// generating hash
+userSchema.methods.generateHash = function (password) {
+    "use strict";
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password valid
+userSchema.methods.validPassword = function (password) {
+    "use strict";
+    return bcrypt.compareSync(password, this.local.password);
+};
 module.exports = mongoose.model('User', userSchema);
