cf7dad4125ac3d1eae0145b63e03454846e68be6
baseplate <puneet.devel@gmail.com>
2014-10-18 17:20:04 +0530

    passport configuration for local login

--- config/passport.js
+++ config/passport.js
@@ -60,4 +60,23 @@ module.exports = function (passport) {
             });
         }
         ));
+
+    passport.use('local-login', new LocalStrategy({usernameField: 'email',
+                                                   passwordField: 'password',
+                                                   passReqToCallback: true },
+    function (req, email, password, done) {
+        User.findOne({'email': email}, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, req.flash('info', 'No user found'));
+            }
+
+
+            if (!user.validPassword(password)) {
+                return done(null, false, req.flash('info', 'Oops !! Wrong password'));
+            }
+            return done(null, user);
+        });
+    }
+    ));
 };
