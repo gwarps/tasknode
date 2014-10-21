773f5d5b94a469fc93914696d56238fd5171692c
Puneet Singh <puneet.mir@gmail.com>
2014-10-21 23:49:31 +0530

    deleted files

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['TaskNode'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '8c449c9f6f2401ef95d0ee8b6a994f09bcfa6182',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
