798c825fc8f5e19a733e2c0b0806b16e724b66a7
touchps <puneet.mir@gmail.com>
2014-11-03 01:13:22 +0530

    grunt-jslint task added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,32 @@
 "use strict"
 
-module.exports = function(grunt) {
-    // Project Configuration
-    pkg: grunt.file.readJSON('package.json');
 
-    
+var paths = {
+   js: ['*.js', '!bower_components/**', 'models/*.js', 'routes/*.js'],
+   jade: ['views/*'],
+   css: ['!bower_components/**', 'public/*.css']
+}
+
+module.exports = function(grunt) {
+
+    // Module load
+    grunt.loadNpmTasks('grunt-jslint');
+    // Project Configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        'jslint' : {
+            server: grunt.file.expand(paths.js),
+            directives: { node: true, todo: true },
+        }
+    });
+/**
+    grunt.registerTask('jslint', 'Apply JSLint check on js files', function() {
+        var next = this.async();
+        grunt.log.write('Loggin something...').ok();
+        grunt.log.write('Loggin something...').error();
+        var fileList = grunt.file.expand(paths.js);
+        grunt.log.ok(fileList);
+    });
+**/
+    grunt.registerTask('default', 'jslint');
 };
